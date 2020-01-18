fd89619478b884fae523366855d2de52a8b09680
Catalina Williams <catalina.rose1361@gmail.com>
2020-01-18 11:08:57 -0700

    added array variables for reservations and waitlist

---
+++ script.js
@@ -0,0 +1,56 @@
+var reservations = [
+    {
+      name: " ", 
+      numberInParty: " ",
+      table: "One"  
+     
+    },
+    {
+      name: " ",
+      numberInParty: " ",
+      table: "Two" 
+     
+    },
+    {
+      name: " ",
+      numberInParty: " ",
+      table: "Three" 
+      
+    },
+    {
+      name: " ",
+      numberInParty: " ",
+      table: "Four"
+      
+    },
+    {
+      name: " ",
+      numberInParty: " ",
+      table: "Five"
+     
+    },
+  
+  ];
+
+  var waitlist = [
+    {
+      name: " ",
+      numberInParty: " ",
+    },
+    {
+      name: " ",
+      numberInParty: " ",
+    },
+    {
+      name: " ",
+      numberInParty: " ",
+    },
+    {
+      name: " ",
+      numberInParty: " ",
+    },
+    {
+      name: " ",
+      numberInParty: " ",
+    }
+]
