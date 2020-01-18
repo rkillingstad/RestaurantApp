fcc649a592e6d2845d1e7aa5bc108539cf70471f
Hayley Budzinski <hayleybudzinski@gmail.com>
2020-01-18 10:48:48 -0700

    returning listening on 3000

---
+++ script.js
@@ -0,0 +1,17 @@
+var express = require("express");
+var path = require("path");
+
+var app = express();
+var PORT = 3000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+var patrons = [{
+    
+}]
+//should listen on port 3000 and console log listening on 3000
+
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
