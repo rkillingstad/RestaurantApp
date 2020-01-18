cd832039e30fa9063c59b909c3f94f7d35fa182e
Hayley Budzinski <hayleybudzinski@gmail.com>
2020-01-18 12:32:11 -0700

    initial code

--- script.js
+++ script.js
@@ -65,11 +65,24 @@ var PORT = 3000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-var patrons = [{
-    
-}]
 //should listen on port 3000 and console log listening on 3000
 
 app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
+  });
+
+
+// Create New Reservation - takes in JSON input
+app.post("/reservations", function(req, res) {
+    // req.body hosts is equal to the JSON post sent from the user
+    // This works because of our body parsing middleware
+    var rewRes = req.body;
+    // Using a RegEx Pattern to remove spaces from rewRes
+    rewRes.routeName = rewRes.name.replace(/\s+/g, "").toLowerCase();
+    
+    console.log(rewRes);
+  
+    reservations.push(rewRes);
+  
+    res.json(rewRes);
   });
